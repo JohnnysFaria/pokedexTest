0225026aa5b41e01d41f33d45acb9c7d98bb72e6
Johnny Faria <johnnys.faria@gmail.com>
2022-03-28 23:44:36 -0300

    scroll infinito

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,31 +4,41 @@ import api from '../../services/api.js';
 import styles from './List.module.css';
 
 const List = () => {
-  const [pages, setPages] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const [infinite, setInfinite] = useState([]);
+  const [offset, setOffset] = useState(-20);
   const [pokemons, setPokemons] = useState([]);
 
-  const getPokemons = async () => {
-    await api
-      .get(`pokemon?limit=6&offset=${offset}`)
-      .then((res) => {
-        setOffset(offset + 6);
-        setPokemons([...pokemons, ...res.data.results]);
-      })
-      .catch((err) => console.log(err));
-  };
+  console.log(offset);
 
   useEffect(() => {
+    const getPokemons = async () => {
+      await api
+        .get(`pokemon?limit=20&offset=${offset}`)
+        .then((res) => {
+          setPokemons([...pokemons, ...res.data.results]);
+        })
+        .catch((err) => console.log(err));
+    };
     getPokemons();
+  }, [offset]);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) {
+        console.log('elemento está visivel', offset);
+        setOffset((offsetInsideState) => offsetInsideState + 20);
+      }
+    });
+    intersectionObserver.observe(document.querySelector('#sentinela'));
+    return () => intersectionObserver.disconnect();
   }, []);
+
   return (
     <>
       <section className={styles.list}>
         {pokemons && pokemons.map((item) => <Card name={item.name} />)}
       </section>
       <div
-        onClick={() => getPokemons()}
+        id="sentinela"
         style={{ background: '#fb1', width: '100%', padding: '20px' }}
       >
         Puxa mais
